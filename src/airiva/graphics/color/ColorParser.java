d953ed12bd94aed505714369180ce7f00c68a0c0
Sawerick <tominek9@centrum.sk>
2013-11-03 17:21:26 +0100

    Added documentation

--- src/airiva/graphics/color/ColorParser.java
+++ src/airiva/graphics/color/ColorParser.java
@@ -6,14 +6,14 @@ public class ColorParser {
 
     private static ColorValidator cv = new ColorValidator();
     private String hexForm;
-    private int R, G, B;
+    private byte R, G, B;
     
     public ColorParser(String hexForm) throws InvalidColorFormatException
     {
         if (!cv.isHexFormValid(hexForm))
             throw new InvalidColorFormatException(
                     "Invalid color hexadecimal representation. ");
-        int[] rgbArr = parseHexForm(hexForm);
+        byte[] rgbArr = parseHexForm(hexForm);
         R = rgbArr[0];
         G = rgbArr[1];
         B = rgbArr[2];
@@ -21,7 +21,7 @@ public class ColorParser {
     
     public ColorParser(Color colorName)
     {
-        int[] rgbArr = parseHexForm(colorName.getHexForm());
+        byte[] rgbArr = parseHexForm(colorName.getHexForm());
         R = rgbArr[0];
         G = rgbArr[1];
         B = rgbArr[2];
@@ -37,31 +37,57 @@ public class ColorParser {
                            Integer.toString(G, 16);
     }
     
-    private int[] parseHexForm(String hexForm)
+    private byte[] parseHexForm(String hexForm)
     {
-        int[] resultArr = new int[3];
+        byte[] resultArr = new byte[3];
         int startIndex = 0;
         for(int i = 0; i < 3; i++)
-            resultArr[i] = Integer.parseInt(hexForm.substring(
+            resultArr[i] = Byte.parseByte(hexForm.substring(
                                     startIndex, (startIndex += 2)), 16);
         return resultArr;
     }
-    
+   
+    /** Will return hexadecimal representation of color as string
+     * @return string containing hexadecimal representation of color
+     */
     public String getHexForm()
     {
         return hexForm;
     }
     
-    public int getR()
+    /** Will return hexadecimal representation of color, (improved
+     * with additional flags)
+     * @param includingHash hex. repr. will contains # at the beggining,
+     * f.e. #00ffff
+     * @param lowercase possible numbers will be lowercase if true, otherwise
+     * uppercase, f.e. true -> 00ffff, false -> 00FFFF
+     * @return hex. representation of color
+     */
+    public String getHexForm(boolean includingHash, boolean lowercase)
+    {
+        return "#" + hexForm.toUpperCase();
+    }
+     
+    /** Will return value of Red in color
+     * @return byte(0-255) value of Red in color
+     */
+    public byte getR()
     {
         return R;
     }
-     
-   public int getG()
+
+    /** Will return value of Green in color
+     * @return byte(0-255) value of Green in color
+     */    
+   public byte getG()
     {
         return G;
     }
-    public int getB()
+   
+   /** Will return value of Blue in color
+    * @return byte(0-255) value of Blue in color
+    */
+    public byte getB()
     {
         return B;
     }
